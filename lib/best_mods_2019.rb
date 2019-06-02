d0e1c73c7c518860071a32b4c47c0cdc5e95908b
Dennis Chung <chungdennis6@gmail.com>
2019-06-02 12:50:17 +0000

    first commit

---
+++ lib/best_mods_2019.rb
@@ -0,0 +1,6 @@
+require "best_mods_2019/version"
+
+module BestMods2019
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/best_mods_2019/version.rb
@@ -0,0 +1,3 @@
+module BestMods2019
+  VERSION = "0.1.0"
+end
